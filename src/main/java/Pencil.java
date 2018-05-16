f8e4679e9988d4f319dc36d2f0aae999f4cd5ef6
cmduncan <cmduncan@iastate.edu>
2018-05-16 13:14:40 -0500

    Tip durability is reduced appropriately by character

--- src/main/java/Pencil.java
+++ src/main/java/Pencil.java
@@ -15,12 +15,21 @@ public class Pencil {
 		}
 	}
 	
-	private void appendCharToPaper(char thisCharacter){
+	private void appendCharToPaper(char characterToAppend){
 		if(this.tipDurability <= 0) {
 			paper.content += ' ';
 		} else {
-			tipDurability -= 1;
-			paper.content += thisCharacter;
+			reducePencilDurabilityBy(characterToAppend);
+			paper.content += characterToAppend;
 		}	
+	}
+	
+	private void reducePencilDurabilityBy(char letterThatWasWritten) {
+		if(Character.isUpperCase(letterThatWasWritten)) {
+			this.tipDurability -= 2;
+		}
+		else if(Character.isLowerCase(letterThatWasWritten)) {
+			this.tipDurability -= 1;
+		}
 	}
 }

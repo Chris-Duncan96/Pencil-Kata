b06c9a66258ce388b31ec800ff394008619a6cd0
cmduncan <cmduncan@iastate.edu>
2018-05-16 16:38:23 -0500

    Added tests to cover insertion (called editing in requirements)

--- src/test/java/PencilTests.java
+++ src/test/java/PencilTests.java
@@ -20,6 +20,48 @@ class PencilTests {
 		paper = new Paper();
 		pencil = new Pencil(paper, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE);
 	}
+	
+	@Test
+	void insertingThreeCharactersOverSomeDifferentCharacterResultsInPartialCollisionTest() {
+		paper.content = "abc";
+		pencil.insert("xbx", 0);
+		assertEquals("@b@", paper.content);
+	}
+	
+	@Test
+	void insertingThreeCharactersOverDifferentCharacterResultsInCollisionTest() {
+		paper.content = "abc";
+		pencil.insert("bcd", 0);
+		assertEquals("@@@", paper.content);
+	}
+	
+	@Test
+	void insertingCharacterOverDifferentCharacterResultsInCollisionTest() {
+		paper.content = "a";
+		pencil.insert("b", 0);
+		assertEquals("@", paper.content);
+	}
+	
+	@Test
+	void insertingCharacterOverSameCharacterDoesNotResultaInCollisionTest() {
+		paper.content = "a";
+		pencil.insert("a", 0);
+		assertEquals("a", paper.content);
+	}
+	
+	@Test
+	void insertingCharacterDoesNotShiftTextTest() {
+		paper.content = " b";
+		pencil.insert("a", 0);
+		assertEquals("ab", paper.content);
+	}
+	
+	@Test
+	void insertingCharacterOverWhitespaceResultsInOverwrittenWhitespaceTest() {
+		paper.content = "a c";
+		pencil.insert("b", 1);
+		assertEquals("abc", paper.content);
+	}
 
 	@Test
 	void eraserDepleatesThrouhoutEraseCallAndStopsIfInsufficientDurabilityTest() {

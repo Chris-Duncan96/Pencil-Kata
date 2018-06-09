0f066a3fdb14214d72420cb7e7f5a07cd13a183f
cmduncan <cmduncan@iastate.edu>
2018-06-09 14:21:38 -0500

    Removed unneeded class variable from pencil

--- src/main/java/Pencil.java
+++ src/main/java/Pencil.java
@@ -2,12 +2,10 @@ package main.java;
 
 public class Pencil {
 
-	public Paper paper;
 	public Eraser eraser;
 	public Writer writer;
 	
 	public Pencil (Paper argPaper, int argTipDurability, int argLength, int argEraserDurability) {
-		this.paper = argPaper;
 		this.writer = new Writer(argPaper, argLength, argTipDurability);
 		this.eraser = new Eraser(argPaper, argEraserDurability);
 	}
